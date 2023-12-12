2e1490123c40dadb99413f77f7c164b636447991
BDunnCode <BDunnCode@github.com>
2023-12-12 14:14:34 -0600

    Stable working

--- index.js
+++ index.js
@@ -33,10 +33,25 @@ app.post('/api', (request, response) => {
 
 // I believe this route gets information from the client
   // No, it should be a get request towards the API_URL, so from the weather API.
+    // It's definitely an endpoint that allows the client side to get a weather object using an HTTP get request. I'm fairly 
+    // sure about that, but I think it does other things. It's like when someone on an approved client side makes a get request
+    // to this endpoint, it will prompt a function that makes a get request to the weather API, which is captures inside of fetch
+    // response, and then sends that fetch response over to original maker of the get request in the first place. This is poorly
+    // worded, but fairly correct.
+    
+      // After that portion comes parameters being added in the routing which is what's going to allow for, at least in this case,
+      // variables to be passed along via those HTTP requests as far as I can tell. The latitude and longitude pulled from the 
+      // geolocation tool inside of the index should get send along with the get request, giving a dynamic latitude and longitude
+      // that will then be sent over to the open weather API, allowing the weather to be passed to the client side.
 
-app.get('/weather/', async (request, response) => {
-  // const api_url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${api_Key}`
-  const api_url = `https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=94.04&appid=b6254fd4b457e5aa978aa18673562b99`
+app.get('/weather/:latlon', async (request, response) => {
+  console.log(request.params)
+  const latlon = request.params.latlon.split(',')
+  console.log(latlon)
+  const lat = latlon[0]
+  const lon = latlon[1]
+  console.log(lat, lon)
+  const api_url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=b6254fd4b457e5aa978aa18673562b99`
   const fetch_response = await fetch(api_url)
   const json = await fetch_response.json()
   response.json(json)
