e575c278f810e601927cea2967d31de2ea0fdc5b
BDunnCode <BDunnCode@github.com>
2023-12-16 17:21:07 -0600

    added more api jank compensation and some styling

--- public/sketch.js
+++ public/sketch.js
@@ -8,6 +8,7 @@ if ('geolocation' in navigator) {
         const api_url = `weather/${lat},${lon}`
         const response = await fetch(api_url)
         const json = await response.json()
+        console.log(json)
         const fahrenheit = ((json.weather.current.temp - 273.15) * 9/5 + 32).toFixed()
         const weather1 = json.weather.current
         document.getElementById('latitude').textContent = lat                  
@@ -15,20 +16,36 @@ if ('geolocation' in navigator) {
         document.querySelector('#temp').textContent = fahrenheit
         document.querySelector('#summary').textContent = weather1.weather[0].main.toLowerCase()
         if (json.air_quality.results[0]) {
-          const air_quality1 = json.air_quality                                
+          const air_quality1 = json.air_quality
+           // Date Translation Code
+          const timestampforcomputers = json.air_quality.results[0].measurements[0].lastUpdated
+          const timestampmidtranslation = new Date(timestampforcomputers)
+          const dateconfig = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric', 
+            hour: 'numeric', 
+            minute: 'numeric', 
+            second: 'numeric', 
+            timeZoneName: 'short' 
+          }
+        const formattedtimedateforhumans = new Intl.DateTimeFormat('en-US', dateconfig).format(timestampmidtranslation)
+        console.log(formattedtimedateforhumans)                                
           document.querySelector('#value').textContent = air_quality1.results[0].measurements[0].value
           document.querySelector('#unit').textContent = air_quality1.results[0].measurements[0].unit
           document.querySelector('#parameter').textContent = air_quality1.results[0].measurements[0].parameter
-          document.querySelector('#timestamp').textContent = air_quality1.results[0].measurements[0].lastUpdated
+          document.querySelector('#timestamp').textContent = formattedtimedateforhumans
         } else {
-          document.querySelector('#test').textContent = 
-          'Unfortunately, the air quality measurement tools are unable to find any data for this area.'
+          document.querySelector('#value').textContent = `"No Satellite Data Found". Unfortunately, the air quality measurement tools are unable to find any data for this area.`
+          const spansToHide = document.querySelectorAll('.hideifnoaq')
+          console.log(spansToHide)
+          spansToHide.forEach((spanElement) => {
+            if (spanElement.textContent.includes('of')) {
+              spanElement.classList.add('hide')
+            }
+          })
+
         }
-        console.log(json.air_quality.results[0].measurements[0].value)
-        console.log(json)
-        console.log(json.air_quality.results[0].measurements[0].parameter)
-        console.log(json.air_quality.results[0].measurements[0].unit)
-        console.log(json.air_quality.results[0].measurements[0].lastUpdated)
     })               
 } else {
   console.log('geolocation not available')
@@ -49,3 +66,4 @@ button.addEventListener('click', async event => {
   const json = await response.json()
   console.log(json)
 })
+
