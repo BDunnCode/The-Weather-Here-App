9462f411d9e1b8c53c542f6eea55037257304108
BDunnCode <BDunnCode@github.com>
2023-12-17 16:04:35 -0600

    Integrating Map, no horrible breaks at this point

--- public/logs/logs.js
+++ public/logs/logs.js
@@ -1,17 +1,37 @@
+const mapContainer = document.getElementById('themap')
+const mymap = L.map(mapContainer).setView([0, 0], 1)
+const attribution = '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+const tiles = L.tileLayer(tileUrl, { attribution })
+tiles.addTo(mymap)
+
+
 getData()
+
 async function getData() {
   const response = await fetch('/api')
   const data = await response.json()
-
+  console.log
   for (item of data) {
-  const root = document.createElement('div')
-  const geo = document.createElement('div')
+
+    const marker = L.marker([item.lat, item.lon]).addTo(mymap)
+    const txt = `The weather here at ${item.lat}°,${item.lon}° `
+    
+    // ${weather.summary} with a temperature of 
+    // ${weather.temp}°F
+
+  // Now let's check on the air quality. OpenAQ API shows a reading of ${air.value}${air.unit} of ${air.parameter} ${air.lastUpdated}`
+
+  marker.bindPopup(txt)
+
+  // const root = document.createElement('div')
+  // const geo = document.createElement('div')
   
-  // quote.innerText = `random text: ${item.quote}`
-  geo.textContent = `${item.lat}°, ${item.lon}°`
+  // geo.textContent = `${item.lat}°, ${item.lon}°`
   
-  root.append(geo) // 
-  document.body.append(root)
+  // root.append(geo) // 
+  // document.body.append(root)
 }
   console.log(data)
 }
+
--- public/sketch.js
+++ public/sketch.js
@@ -1,5 +1,5 @@
 // Geo Locate
-let lat, lon
+let lat, lon, air, weather
 if ('geolocation' in navigator) {                                         
       console.log('geolocation available')                                    
       navigator.geolocation.getCurrentPosition(async position => {                                                                                       
@@ -10,13 +10,13 @@ if ('geolocation' in navigator) {
         const json = await response.json()
         console.log(json)
         const fahrenheit = ((json.weather.current.temp - 273.15) * 9/5 + 32).toFixed()
-        const weather1 = json.weather.current
+        weather = json.weather.current
         document.getElementById('latitude').textContent = lat                  
         document.getElementById('longitude').textContent = lon
         document.querySelector('#temp').textContent = fahrenheit
-        document.querySelector('#summary').textContent = weather1.weather[0].main.toLowerCase()
+        document.querySelector('#summary').textContent = weather.weather[0].main.toLowerCase()
         if (json.air_quality.results[0]) {
-          const air_quality1 = json.air_quality
+          air = json.air_quality.results[0].measurements[0]
            // Date Translation Code
           const timestampforcomputers = json.air_quality.results[0].measurements[0].lastUpdated
           const timestampmidtranslation = new Date(timestampforcomputers)
@@ -31,9 +31,9 @@ if ('geolocation' in navigator) {
           }
         const formattedtimedateforhumans = new Intl.DateTimeFormat('en-US', dateconfig).format(timestampmidtranslation)
         console.log(formattedtimedateforhumans)                                
-          document.querySelector('#value').textContent = air_quality1.results[0].measurements[0].value
-          document.querySelector('#unit').textContent = air_quality1.results[0].measurements[0].unit
-          document.querySelector('#parameter').textContent = air_quality1.results[0].measurements[0].parameter
+          document.querySelector('#value').textContent = json.air_quality.results[0].measurements[0].value
+          document.querySelector('#unit').textContent = json.air_quality.results[0].measurements[0].unit
+          document.querySelector('#parameter').textContent = json.air_quality.results[0].measurements[0].parameter
           document.querySelector('#timestamp').textContent = formattedtimedateforhumans
         } else {
           document.querySelector('#value').textContent = `"No Satellite Data Found". Unfortunately, the measurement tools are unable to find any air quality data for this area. You can consult the about page for more information.`
@@ -51,10 +51,12 @@ if ('geolocation' in navigator) {
   console.log('geolocation not available')
 }
 
+
 // Handle button presses, submit data to database
 const button = document.getElementById('checkin')
 button.addEventListener('click', async event => {
-  const data = { lat, lon }
+  console.log(lat, lon, weather, air)
+  const data = { lat, lon, weather, air }
   const options = {
     method: "POST",
     headers: {
@@ -62,8 +64,8 @@ button.addEventListener('click', async event => {
     },
     body: JSON.stringify(data)
   }
-  const response = await fetch('/api', options)
-  const json = await response.json()
-  console.log(json)
+  const db_response = await fetch('/api', options)
+  const db_json = await db_response.json()
+  console.log(db_json)
 })
 
