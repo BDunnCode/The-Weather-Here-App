91d06d31ce40b041550a3f029be015abffa5b25e
BDunnCode <BDunnCode@github.com>
2023-12-18 20:28:32 -0600

    update to latest working state

--- public/logs/logs.js
+++ public/logs/logs.js
@@ -15,23 +15,24 @@ async function getData() {
   for (item of data) {
 
     const marker = L.marker([item.lat, item.lon]).addTo(mymap)
-    const txt = `The weather here at ${item.lat}°,${item.lon}° `
+    const txt = `
+    The weather here at ${item.lat}°,${item.lon}° shows ${item.weather.weather[0].main.toLowerCase()}, with a temperature
+    of ${((item.weather.temp - 273.15) * 9/5 + 32).toFixed()}°F. Now let's get a reading for the air quality. OpenAQ API shows a 
+    reading of ${item.air.value}${item.air.unit} of ${item.air.parameter} ${item.air.lastUpdated}`
+
+    // The problems with the OpenAQ API break the functionality on this and seem to cause the loop to error out and stop working.
+    // Might want to concatenate the air quality portion second because it's gonna cause all kinds of problems.
     
-    // ${weather.summary} with a temperature of 
-    // ${weather.temp}°F
-
-  // Now let's check on the air quality. OpenAQ API shows a reading of ${air.value}${air.unit} of ${air.parameter} ${air.lastUpdated}`
-
   marker.bindPopup(txt)
 
-  // const root = document.createElement('div')
-  // const geo = document.createElement('div')
-  
-  // geo.textContent = `${item.lat}°, ${item.lon}°`
-  
-  // root.append(geo) // 
-  // document.body.append(root)
 }
   console.log(data)
 }
 
+// const root = document.createElement('div')
+// const geo = document.createElement('div')
+
+// geo.textContent = `${item.lat}°, ${item.lon}°`
+
+// root.append(geo) // 
+// document.body.append(root)
