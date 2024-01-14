9cc2b851aa8588d307b40cb090400ada387a982c
BDunnCode <BDunnCode@github.com>
2024-01-13 19:52:52 -0600

    Cleaned up some things with dates.

--- public/logs/logs.js
+++ public/logs/logs.js
@@ -13,6 +13,7 @@ async function getData() {
   const data = await response.json()
   console.log(data)
   for (item of data) {
+
     const marker = L.marker([item.lat, item.lon]).addTo(mymap)
     
     let airText = '' 
@@ -22,8 +23,24 @@ async function getData() {
     }
 
     if (item.foundObject != undefined) {
+      // The line below is the timestamp returned from the AQ API, it comes in ISO 8601 format as a string
+      const unformattedTimestamp = item.foundObject.lastUpdated
+      // Below takes the string format and makes a date object with it
+      const timestampMidTranslation = new Date(unformattedTimestamp)
+      // This is the config that will determine the readout given when using new Intl.DateTimeFormat and its
+      // format method
+       const dateconfig = 
+       {
+        month: 'long',
+        day: 'numeric', 
+        year: 'numeric',
+      }
+      // This completes the variable and allows for better readability for the user.
+      const formattedTimestamp = new Intl.DateTimeFormat('en-US', dateconfig).format(timestampMidTranslation)
+
       airText = `Now let's get a reading for the air quality. OpenAQ API shows a 
-      reading of ${item.foundObject.value}${item.foundObject.unit} of ${item.foundObject.parameter} as of the last update on ${item.foundObject.lastUpdated}`
+      reading of ${item.foundObject.value}${item.foundObject.unit} of ${item.foundObject.parameter} 
+      as of the last update on ${formattedTimestamp}`
     }
   
     const txt = `
